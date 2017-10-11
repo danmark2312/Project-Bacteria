7e851d3de17a5f8d66e2ff45cfd3f9fd75389d3f
Simon Moe <moe.simon@gmail.com>
2017-10-11 18:21:40 +0200

    Added comments

--- functions/userinput.py
+++ functions/userinput.py
@@ -11,6 +11,16 @@ import numpy as np
 def inputStr(prompt):
     """
     Userinput that only allows strings
+
+    INPUT:
+        prompt: String
+
+    OUTPUT:
+        str: The inputted string
+
+    USAGE:
+        inputStr("Please enter a string: ")
+
     """
     while True:
         try:
@@ -36,24 +46,24 @@ def displayMenu(options):
     """
     DISPLAYMENU Displays a menu of options, ask the user to choose an item
     and returns the number of the menu item chosen.
-    
+
     Usage: choice = displayMenu(options)
-    
+
     Input options Menu options (array of strings)
     Output choice Chosen option (integer)
-    
-    
+
+
     Author: Mikkel N. Schmidt, mnsc@dtu.dk, 2015
     """
-    
+
     # Display menu options
     for i in range(len(options)):
         print("{:d}. {:s}".format(i+1, options[i]))
-        
+
     # Get a valid menu choice
     choice = 0
-    
+
     while not(np.any(choice == np.arange(len(options))+1)):
        choice = inputNumber("Please choose a menu item: ")
-       
+
     return choice
